51893ad9bfb3797d941c40e16dfae9786c3a0706
unknown <md.asif.8393@gmail.com>
2023-08-10 22:00:23 +0600

    database connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello world!"); 
+import express, { Application } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+const app: Application = express();
+
+//using cors
+app.use(cors());
+
+//parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const { URI } = process.env;
+const port = process.env.PORT || 5000;
+console.log(URI);
+const dbconnect = async () : Promise<void> => {
+    if(!URI){
+        throw new Error("URI is not defined");
+    }
+    await mongoose.connect(URI)
+    .then(() =>console.log("Database connected"));
+}
+
+dbconnect();
+
